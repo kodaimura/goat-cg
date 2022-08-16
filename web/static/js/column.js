cfa92d8f0a544eb1ad7ef17923b4b6831ffda75d
kodaimura <kodaimura123@gmail.com>
2022-08-16 23:27:11 +0900

    js input control

--- web/static/js/column.js
+++ web/static/js/column.js
@@ -1,6 +1,20 @@
-document.getElementById("modal-del-button").addEventListener("click", (e)=>{
-	fetch("", {method: "DELETE"})
-	.then(data => {
-		window.location = "../columns"
-	})
+document.getElementById("data_type_cls").addEventListener("change", (e)=>{
+	switch (e.target.value) {
+		case "11":
+		case "12":
+			document.getElementById("precision").disabled = false
+			document.getElementById("scale").disabled = true
+			document.getElementById("scale").value = 0
+			break;
+		case "30":
+			document.getElementById("precision").disabled = false
+			document.getElementById("scale").disabled = false
+			break;
+		default:
+			document.getElementById("precision").disabled = true
+			document.getElementById("scale").disabled = true
+			document.getElementById("precision").value = 0
+			document.getElementById("scale").value = 0
+			break;
+	}
 })
