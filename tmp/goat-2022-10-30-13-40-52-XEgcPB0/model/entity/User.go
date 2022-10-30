aaa205cbaa82c96e6e7e28622d3e1e6d4c5b885a
kodaimura <kodaimura123@gmail.com>
2022-10-30 13:44:54 +0900

    remove unnecessary files

--- tmp/goat-2022-10-30-13-40-52-XEgcPB0/model/entity/User.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-
-type User struct {
-	UserId int `db:"user_id" json:"user_id"`
-	TestColumn string `db:"test_column" json:"test_column"`
-	CreateAt string `db:"create_at" json:"create_at"`
-	UpdateAt string `db:"update_at" json:"update_at"`
-}
